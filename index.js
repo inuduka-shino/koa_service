56b4ff4d41bd0c5fd9e4e0eb2447a970f14fd2d1
n.m.mobile <hihinuma@gmail.com>
2017-04-19 07:53:03 +0900

    start message

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 /*eslint-env node */
+/*eslint no-console: off*/
 'use strict';
 
 import Koa from 'koa';
+import http from 'http';
+
 const app = new Koa();
 
 // アクセスログ
@@ -9,10 +12,11 @@ app.use(async (ctx, next) => {
   const start = new Date;
   await next();
   const ms = new Date - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  console.log(`${ctx.method} ${ctx.url} ${ctx.status} - ${ms}ms `);
 });
 
 // エラーハンドリング
+// TODO: BOOMを使ってみる
 app.use(async (ctx, next) => {
   try {
     await next();
@@ -28,4 +32,24 @@ app.use(async (ctx, next) => {
   ctx.body = 'Hello world!';
 });
 
-app.listen(3000);
+//app.listen(3000);
+let port = 3000;
+http.createServer(app.callback()).listen(port, () => {
+  console.log(`start http service on ${port} port.`);
+
+  console.log('ip address');
+  const os=require('os');
+  const interfaces = os.networkInterfaces();
+
+  for (let dev in interfaces) {
+    const devDetails = interfaces[dev];
+
+    devDetails.forEach((detail) => {
+      // console.log(devDetail);
+      if (detail.family === 'IPv4') {
+          console.log(detail.address);
+      }
+    });
+  }
+
+});
