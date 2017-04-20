20c59a310939ee445ce35618eadfbf9da84955ce
n.m.mobile <hihinuma@gmail.com>
2017-04-21 08:07:39 +0900

    ect

--- apps/router.js
+++ apps/router.js
@@ -1,10 +1,15 @@
+/*eslint-env node */
 import Router from 'koa-router';
 
 const router = new Router();
+var ECT = require('ect');
+var renderer = ECT({ root : __dirname + '/views' });
+var data = { title : 'Hello, ECT World!' };
+var html = renderer.render('template.ect', data);
 
 // top page
 router.get('/', (ctx) => {
-  ctx.body = 'top page';
+  ctx.body = html;
 });
 
 export default router;
