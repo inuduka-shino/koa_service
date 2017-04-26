7179905edb1eb04e0f63e1713fcaddac4baffe42
n.m.mobile <hihinuma@gmail.com>
2017-04-27 06:53:07 +0900

    static page

--- apps/staticlib.js
+++ apps/staticlib.js
@@ -6,11 +6,9 @@ import serv from 'koa-static';
 
 const app = new Koa();
 
-app.use(serv(path(__dirname, '/static')));
-/*
-app.use(serv(path(__dirname, '/static'), {
+app.use(serv(path.join(__dirname, '/static'), {
   index: 'index.html',
   extensions: ['html']
-}));*/
+}));
 
 export default app;
